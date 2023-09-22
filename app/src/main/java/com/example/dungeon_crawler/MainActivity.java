f9d01461d332a694986cffd5eaec1b5add754c70
emmalynguyen <emmalycnguyen@gmail.com>
2023-09-22 13:14:12 -0400

    Image in background, text and button on screen with font
    Image in background, text and button on screen with font

--- app/src/main/java/com/example/dungeon_crawler/MainActivity.java
+++ app/src/main/java/com/example/dungeon_crawler/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 
@@ -13,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.welcome);
         Button startBtn = findViewById(R.id.startButton);
 
         // Set difficulty based on difficulty checked
