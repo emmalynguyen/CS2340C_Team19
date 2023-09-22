cdbd89aae6f4d67282c43bc7492021a12d31a181
ColeKaplan <colegkaplan@gmail.com>
2023-09-22 13:43:50 -0400

    Images look the same between scenes
    Images look the same between scenes

--- app/src/main/java/com/example/dungeon_crawler/MainActivity.java
+++ app/src/main/java/com/example/dungeon_crawler/MainActivity.java
@@ -16,19 +16,8 @@ public class MainActivity extends AppCompatActivity {
         Button startBtn = findViewById(R.id.startButton);
 
         // Set difficulty based on difficulty checked
-        startBtn.setOnClickListener(v -> {
-            RadioGroup difficultyRadioGroup = findViewById(R.id.difficultyRadioGroup);
-            double difficulty = 1;
+        //startBtn.setOnClickListener(v -> {
 
-            if (difficultyRadioGroup.getCheckedRadioButtonId() == R.id.easy) {
-                difficulty = 0.5;
-            } else if(difficultyRadioGroup.getCheckedRadioButtonId() == R.id.radioMedium) {
-                difficulty = .75;
-            } else if(difficultyRadioGroup.getCheckedRadioButtonId() == R.id.radioHard) {
-                difficulty = 1;
-            } else {
-                difficulty = 1;
-            }
 
             Intent game = new Intent(MainActivity.this, GameActivity.class);
             game.putExtra("difficulty", difficulty);
