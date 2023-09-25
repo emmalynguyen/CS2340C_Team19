9afb7232e1957c95b021685c3ab0732f9c0083d5
ColeKaplan <colegkaplan@gmail.com>
2023-09-24 23:08:22 -0400

    GameScene Updates
    Carry over variables

--- DungeonCrawler/app/src/main/java/com/example/dungeoncrawler/GameScene.java
+++ DungeonCrawler/app/src/main/java/com/example/dungeoncrawler/GameScene.java
@@ -5,35 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class GameScene extends AppCompatActivity {
 
+    String username;
+    double difficulty;
+    int sprite;
+    double health;
+    String difficultyLevel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_scene);
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
-        TextView name = findViewById(R.id.inputText);
-        name.setText("Your Name"); //update with their name
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
-        TextView difficulty = findViewById(R.id.inputText);
-        difficulty.setText("Your Name"); //update with their name
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
-        TextView health = findViewById(R.id.inputText);
-        health.setText("Your Name"); //update with their name
+        String username = extras.getString("name");
+        double difficulty = extras.getDouble("difficulty");
+        int sprite = extras.getInt("sprite");
+        double health = extras.getDouble("health");
+        String difficultyLevel = extras.getString("DifficultyLevel");
+
+        Log.d("tag", "Username: " + username);
+
+        TextView nameTextView = findViewById(R.id.nameText);
+        nameTextView.setText(username);
+
+        TextView nameTextView = findViewById(R.id.nameText);
+        nameTextView.setText(username);
+//
+//        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+//        TextView difficulty = findViewById(R.id.inputText);
+//        difficulty.setText("Your Name"); //update with their name
+//
+//        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+//        TextView health = findViewById(R.id.inputText);
+//        health.setText("Your Name"); //update with their name
 
         Button endingButton = findViewById(R.id.endingButton);
 
         endingButton.setOnClickListener(v -> {
-            Intent intent = new Intent(GameScene.this, Ending.class);
+            Intent ending = new Intent(GameScene.this, Ending.class);
 
             // Start SecondActivity
-            startActivity(intent);
+            startActivity(ending);
 
             // Finish the current activity if needed
             finish();
