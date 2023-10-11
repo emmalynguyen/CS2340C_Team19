7853504b16421df66102ed7bbe6e85389863bc21
emmalynguyen <emmalycnguyen@gmail.com>
2023-10-11 18:15:51 -0400

    checkstyle clear

--- DungeonCrawler/app/src/main/java/com/example/dungeoncrawler/views/GameSceneMedium.java
+++ DungeonCrawler/app/src/main/java/com/example/dungeoncrawler/views/GameSceneMedium.java
@@ -60,7 +60,7 @@ public class GameSceneMedium extends AppCompatActivity {
             }
 
             @Override
-            public void onFinish() {}
+            public void onFinish() { }
 
         }.start();
     }
