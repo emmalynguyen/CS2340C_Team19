927717677ea4d874d1357237b540efa310d74366
TanaviPatel <tanavipb1122@gmail.com>
2023-11-28 17:38:19 -0500

    Powerups Working

--- DungeonCrawler/app/src/main/java/com/example/dungeoncrawler/models/TeleportationPowerUp.java
+++ DungeonCrawler/app/src/main/java/com/example/dungeoncrawler/models/TeleportationPowerUp.java
@@ -4,8 +4,8 @@ public class TeleportationPowerUp implements PowerUp {
     @Override
     public void applyPowerUp(Player player) {
         // Implements the logic for teleportation
-        int newX = 200; // Set your desired X-coordinate for teleportation
-        int newY = 150; // Set your desired Y-coordinate for teleportation
+        int newX = player.getX() + 300; // Set your desired X-coordinate for teleportation
+        int newY = player.getY() + 300; // Set your desired Y-coordinate for teleportation
 
         player.setX(newX);
         player.setY(newY);
